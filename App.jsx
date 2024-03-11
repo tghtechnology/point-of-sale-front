636a344fc89ca99caee860b5e99bb977f95a0c0e
Angie Carhuas <carhuas2005@gmail.com>
2024-03-11 12:49:41 -0500

    r

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-import Navigation from './Navigation.js';
+import Navigation from './Navigation.jsx';
 
 
 export default function App() {
