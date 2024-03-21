fe06b07d035886a6e406b4ab31652dbfdd599a68
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-03-21 16:45:53 -0500

    Consumo Exitoso

--- screens/context/auth/AuthProvider.jsx
+++ screens/context/auth/AuthProvider.jsx
@@ -1,18 +1,20 @@
 import React,{useState} from "react";
 import AuthContext from "./AuthContext"
 import { createToken } from "../../services/authService";
-import { AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage"
 const AuthProvider = ({children}) => {
     const [isAuth, setIsAuth] = useState(false);
 
     const loginAccess = async (email,password) => {
         const { status, data } = await createToken(email,password);
         if (status === 200) {
-          //AsyncStorage.setItem("token", data.token);
-          //AsyncStorage.setItemAsync('token',data.token);
+          const token=data.token
+          AsyncStorage.setItem("token",token);
+          AsyncStorage.getItem("token");
+          console.log("Token: ",token);
           //const token = await SecureStore.getItemAsync('token');
           setIsAuth(true);
-        
+  
           alert("Autenticación exitosa");
           return true;
         } else {
