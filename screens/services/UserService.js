cf4c82cc97c0adae2b801627d07c60efc49fd5b8
tghtechnology <146979530+tghtechnology@users.noreply.github.com>
2024-03-26 08:20:39 -0500

    verificar

--- screens/services/UserService.js
+++ screens/services/UserService.js
@@ -12,6 +12,20 @@ const createUser = async (newUser) => {
     }
 }
 
+const verifyUser = async (id,password) =>{
+    try{
+        const {data, status} = await apiClient.post(`/verificar/${id}`,password);
+        return{
+            data,
+            status
+        }
+    }catch{
+            console.log(error);
+        }
+    }
+
+
 export {
-    createUser
+    createUser,
+    verifyUser
 }
