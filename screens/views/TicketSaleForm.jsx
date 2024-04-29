92699a7583d79353b50009fc51d4ee7f65ae35d3
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-04-29 09:38:07 -0500

    Select Tipo de Pago

--- screens/views/TicketSaleForm.jsx
+++ screens/views/TicketSaleForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, TouchableOpacity, StyleSheet, Text, TextInput } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Text, TextInput, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const TicketSaleForm = (total) => {
@@ -16,29 +16,51 @@ const TicketSaleForm = (total) => {
     return (
         <View style={styles.container}>
             <View style={styles.inputContainer}>
-        <Text style={styles.label}>Ingresa Monto Recibido</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Monto"
-          value={receivedAmount}
-          onChangeText={setReceivedAmount}
-          keyboardType="numeric"
-        />
-      </View>
+                <Text style={styles.label}>Ingresa Monto Recibido</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Monto"
+                    value={receivedAmount}
+                    onChangeText={setReceivedAmount}
+                    keyboardType="numeric"
+                />
+            </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Vuelto</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Valor"
-          value={change}
-          editable={false}
-        />
-      </View>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>Vuelto</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Valor"
+                    value={change}
+                    editable={false}
+                />
+            </View>
 
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Completar Venta</Text>
-      </TouchableOpacity>
+            <View style={styles.itemList}>
+                <View style={styles.item}>
+                    <TouchableOpacity
+                        style={[
+                            styles.circle
+                        ]}
+                    />
+                    <Text style={styles.itemText}>Efectivo</Text>
+                </View>
+            </View>
+
+            <View style={styles.itemList}>
+                <View style={styles.item}>
+                    <TouchableOpacity
+                        style={[
+                            styles.circle
+                        ]}
+                    />
+                    <Text style={styles.itemText}>Tarjeta</Text>
+                </View>
+            </View>
+
+            <TouchableOpacity style={styles.button} onPress={() => { }}>
+                <Text style={styles.buttonText}>Completar Venta</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -89,6 +111,32 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold',
     },
+    //Estilos para el Select
+    itemList: {
+        marginTop: 10,
+    },
+    item: {
+        flexDirection: 'row',
+        padding: 10,
+        alignItems: 'center',
+    },
+    circle: {
+        width: 23.59, // Aumenta el ancho del círculo cuadrado
+        height: 19.59, // Aumenta la altura del círculo cuadrado
+        borderWidth: 2,
+        borderColor: '#517EF2',
+        backgroundColor: '#FFF',
+        marginRight: 10,
+    },
+    circleSelected: {
+        backgroundColor: 'blue', // Color del círculo seleccionado
+        borderColor: 'blue', // Color del borde del círculo seleccionado
+    },
+    itemText: {
+        flex: 1,
+        fontSize: 14,
+    },
+    //
 });
 
 export default TicketSaleForm;
