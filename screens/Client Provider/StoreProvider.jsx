359cb416c60946822a7485543f367cb39feeb7ae
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-05-01 20:56:00 -0500

    Implementacion Storage

--- screens/Client Provider/StoreProvider.jsx
+++ screens/Client Provider/StoreProvider.jsx
@@ -1,17 +1,40 @@
-import { children, createContext, useReducer } from "react";
-import storeReducer, { initialStore } from "./storeReducer";
+import React, { createContext, useContext, useReducer } from "react";
+import storeReducer from "./storeReducer";
 
 const StoreContext = createContext();
 
- const StoreProvider = ({children}) => {
-    const [store, dispatch] = useReducer(storeReducer, initialStore)
+const StoreProvider = ({ children }) => {
+    const [store, dispatch] = useReducer(storeReducer, {});
+
+    const seleccionarProducto = (productId) => {
+        dispatch({ type: 'SELECCIONAR_PRODUCTO', payload: { productId } });
+    };
+
+    const seleccionarDescuento = (discountId) => {
+        dispatch({ type: 'SELECCIONAR_DESCUENTO', payload: { discountId } });
+    };
+
+    const seleccionarCliente = (clientId) => {
+        dispatch({ type: 'SELECCIONAR_CLIENTE', payload: { clientId } });
+    };
+
+    const seleccionarImpuesto = (taxId) => {
+        dispatch({ type: 'SELECCIONAR_IMPUESTO', payload: { taxId } });
+    };
 
     return (
-        <StoreContext.Provider value={[store, dispatch]}>
+        <StoreContext.Provider value={{ store, seleccionarProducto, seleccionarDescuento, seleccionarCliente, seleccionarImpuesto }}>
             {children}
         </StoreContext.Provider>
-    )
- }
+    );
+};
 
- export { StoreContext}
- export default StoreProvider;
+const useStore = () => {
+    const context = useContext(StoreContext);
+    if (!context) {
+        throw new Error('useStore must be used within a StoreProvider');
+    }
+    return context;
+};
+
+export { StoreProvider, useStore };
--- screens/Client Provider/storeReducer.jsx
+++ screens/Client Provider/storeReducer.jsx
@@ -1,18 +1,26 @@
-const initialStore = {
-    user: { id: 1, name: 'luis'},
-    products: [
-        {id: 1, title: 'Product #1'},
-        {id: 2, title: 'Product #2'}
-    ]
-}
-
+import React, { useReducer } from 'react';
 
 const storeReducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
+        case 'SELECCIONAR_PRODUCTO':
+            return { ...state, selectedProductId: action.payload.productId };
+        case 'SELECCIONAR_DESCUENTO':
+            return { ...state, selectedDiscountId: action.payload.discountId };
+        case 'SELECCIONAR_CLIENTE':
+            return { ...state, selectedClientId: action.payload.clientId };
+        case 'SELECCIONAR_IMPUESTO':
+            return { ...state, selectedTaxId: action.payload.taxId };
         default:
             return state;
     }
-}
+};
 
-export { initialStore }
-export default  storeReducer;
+export const TicketFormStoreProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(storeReducer, {});
+
+    return (
+        <TicketFormStoreContext.Provider value={{ state, dispatch }}>
+            {children}
+        </TicketFormStoreContext.Provider>
+    );
+};
