534b356f11a92433838dd61fc2ac7f474c7ad98e
Angie Carhuas <carhuas2005@gmail.com>
2024-05-05 21:51:33 -0500

    .

--- screens/context/sale/SaleProvider.jsx
+++ screens/context/sale/SaleProvider.jsx
@@ -6,13 +6,20 @@ const SaleProvider = ({ children }) => {
   const handleCreateSale = async (newSal) => {
     const {detalles,tipoPago,impuestoId,descuentoId,clienteId,usuarioId,dineroRecibido} = newSal;
     try {
-        const status = await createSale({detalles,tipoPago,impuestoId,descuentoId,clienteId,usuarioId,dineroRecibido});
-        return status === 200 || status === 201;
+        const res = await createSale({detalles,tipoPago,impuestoId,descuentoId,clienteId,usuarioId,dineroRecibido});
+        if (res.status === 200 || res.status === 201){
+          return res.data
+        }else {
+          console.error("Error al crear el venta:", res.status);
+          return null;
+        }
     } catch (error) {
         console.log("Error creating venta:", error);
-        return false;
+        return null;
     }
 };
+
+
 
   return (
     <SaleContext.Provider value={handleCreateSale}> 
