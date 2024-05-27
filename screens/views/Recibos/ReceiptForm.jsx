fea701435874bf5f289fa039f2c141c78e55a6ef
Estefany Torres <estefanyt3101@gmail.com>
2024-05-26 22:44:47 -0500

    New

--- screens/views/Recibos/ReceiptForm.jsx
+++ screens/views/Recibos/ReceiptForm.jsx
@@ -35,12 +35,16 @@ const ReceiptForm = () => {
     }
     return '';
   };
+  function padLeft(number) {
+    return number < 10 ? `0${number}` : number;
+  }
 
   const renderItem = ({ item }) => {
     console.log('Renderizando item:', item);
     const isReembolsado = item.monto_reembolsado !== null;
-    const localDate = moment(item.fecha_creacion).tz('America/Lima').format('DD/MM/YYYY HH:mm:ss');
-    return (
+    const fecha = new Date(item.fecha_creacion);
+    const formattedDate = `${padLeft(fecha.getUTCDate())}-${padLeft(fecha.getUTCMonth() + 1)}-${fecha.getUTCFullYear()} ${padLeft(fecha.getUTCHours())}:${padLeft(fecha.getUTCMinutes())}:${padLeft(fecha.getUTCSeconds())}`;
+        return (
       <TouchableOpacity onPress={() => navigation.navigate('ReceiptDetail', { idRecibo: item.id })}>
         <View style={styles.itemContainer}>
           <View style={styles.iconContainer}>
@@ -51,7 +55,7 @@ const ReceiptForm = () => {
               {isReembolsado ? `S/. ${item.monto_reembolsado}` : `S/. ${getTotal(item.id_venta)}`}
             </Text>
             <Text style={styles.itemText}>
-              {localDate}
+            {formattedDate}
             </Text>
           </View>
           <View style={styles.refContainer}>
